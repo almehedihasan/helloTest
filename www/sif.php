eb9632c7ec876d2cadb648b193e5ec6208133fa0
mehedi <mehedi.cse.ru@gmail.com>
2015-04-27 16:51:26 +0600

    test php file

---
+++ www/sif.php
@@ -0,0 +1,27 @@
+<html >
+<body>
+ID no.:<?php echo $_POST[ "ID"] ?> <br />
+Student Name: <?php echo $_POST["s_name"]?> <br />
+Father's Name:<?php echo $_POST["F_Name"] ?><br />
+Mother's Name:<?php echo $_POST["M_Name"]?> <br />
+Date of birth:<?php echo $_POST["dd"] ?> 
+<?php echo $_POST["mm"] ?> 
+
+<?php echo $_POST["yy"] ?> <br />
+Present address:<?php echo $_POST["Pr_Address"] ?> <br />
+Permanent address:<?php echo $_POST["Per_Address"] ?> <br />
+Contact no: Phone no.:<?php echo $_POST["phone"] ?> <br />
+ Mobile no.:<?php echo $_POST["mobile"] ?> <br />
+e-mail ID:<?php echo $_POST["email"] ?> <br />
+Session:<?php echo $_POST["session"] ?> <br />
+Religeious:<?php echo $_POST["religious"] ?> <br />
+Class:<?php echo $_POST["class"] ?> <br />
+Sex: <?php echo $_POST["Male"]  ?> <br />
+<?php echo $_POST["Female"] ?> <br />
+Nationality:<?php echo $_POST["nationality"] ?> <br />
+
+</form>
+</body>
+</html>
+</body>
+</html>
